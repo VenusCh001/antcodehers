6d09053d619df75e75b6c0d010caf53d492d8bd7
vidhiwalia <vidhiwalia16@gmail.com>
2024-11-15 16:18:34 +0530

    navbar_changed

--- skeleton/notebook/src/App.js
+++ skeleton/notebook/src/App.js
@@ -1,60 +1,78 @@
+
 import './App.css';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
+  useLocation,
 } from "react-router-dom";
+import { useState, useRef, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Alert from './components/Alert';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-
 import NoteSate from './components/context/Notestate';
-import { useState } from 'react';
 import Chatbot from './components/Chatbot';
-import Frontpage from './components/Frontpage';
 import Contact from './components/Contact';
 import LandingPage from './components/LandingPage';
 import Mood_tracker from './components/Mood_tracker';
 import Immersive from './components/immersive';
-
+import LoadingBar from 'react-top-loading-bar';
 
 function App() {
-  const [alert, setAlert] = useState(null)
-  const showAlert=(message,type)=>{
+  const [alert, setAlert] = useState(null);
+  const loadingBarRef = useRef(null); // Reference for Loading Bar
+
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
+      msg: message,
+      type: type
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-  }
+  };
+
+  const handleRouteChange = () => {
+    if (loadingBarRef.current) {
+      loadingBarRef.current.complete(); // Completes loading bar after the route is loaded
+    }
+  };
+
+  const LoadingBarHandler = () => {
+    const location = useLocation(); // This hook gives us access to location change
+
+    useEffect(() => {
+      if (loadingBarRef.current) {
+        loadingBarRef.current.continuousStart(); // Starts the loading bar
+      }
+      handleRouteChange(); // Completes the loading bar after navigating
+    }, [location]); // Executes every time the route changes
+
+    return null; // This component doesn't render anything
+  };
+
   return (
     <>
-
       <NoteSate>
-        
         <Router>
-          <Navbar />
-          
-          {/* <Alert alert={alert}/> */}
+          <Navbar /> {/* Navbar */}
+          <LoadingBarHandler />
+          <LoadingBar color="blue" ref={loadingBarRef} height={3} /> {/* Loading Bar below Navbar */}
+
           <div className='container'>
             <Routes>
-              <Route exact path="/Home" element={<Home showAlert={showAlert} />} />
+              <Route exact path="/home" element={<Home showAlert={showAlert} />} />
               <Route exact path="/" element={<LandingPage showAlert={showAlert} />} />
-              {/* <Route exact path="/LandingPage" element={<LandingPage/>} /> */}
               <Route exact path="/about" element={<About />} />
-              <Route exact path="/chatbot" element={<Chatbot/>} />
-              <Route exact path="/contact" element={<Contact/>} />
-              <Route exact path="/mood-tracker" element={<Mood_tracker/>} />
-              <Route exact path="/immersive" element={<Immersive/>} />
-
-              <Route exact path="/login" element={<Login showAlert={showAlert}/>} />
-              <Route exact path="/signup" element={<SignUp showAlert={showAlert}/>} />
-
+              <Route exact path="/chatbot" element={<Chatbot />} />
+              <Route exact path="/contact" element={<Contact />} />
+              <Route exact path="/mood-tracker" element={<Mood_tracker />} />
+              <Route exact path="/immersive" element={<Immersive />} />
+              <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+              <Route exact path="/signup" element={<SignUp showAlert={showAlert} />} />
             </Routes>
           </div>
         </Router>
